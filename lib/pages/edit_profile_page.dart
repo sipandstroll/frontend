372190ec6da6784a711b58579e3baee6adc8b747
Pericica Iustin <iustin.pericica@selfpay.ro>
2022-06-12 00:37:36 +0300

    feat(edit-profile): pick/ change image LOCAL STATE ONLY

--- lib/pages/edit_profile_page.dart
+++ lib/pages/edit_profile_page.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:image_picker/image_picker.dart';
 
 class EditProfilePage extends StatefulWidget {
   const EditProfilePage({Key? key}) : super(key: key);
@@ -9,10 +13,12 @@ class EditProfilePage extends StatefulWidget {
 }
 
 class _EditProfilePageState extends State<EditProfilePage> {
+  XFile? image;
+  final ImagePicker _picker = ImagePicker();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        extendBodyBehindAppBar: true,
         appBar: AppBar(
           elevation: 0,
           leading: IconButton(
@@ -24,7 +30,47 @@ class _EditProfilePageState extends State<EditProfilePage> {
               context.pop();
             },
           ),
-          title: const Text('Edit info'),
+          title: const Text('Edit inf1o'),
+        ),
+        body: Column(
+          children: [
+            Container(
+              child: image == null
+                  ? TextButton(
+                      onPressed: getImage,
+                      child: Text(
+                        'Pick image',
+                        style: Theme.of(context)
+                            .textTheme
+                            .subtitle2
+                            ?.copyWith(color: Colors.blueGrey),
+                      ),
+                    )
+                  : Column(
+                      children: [
+                        Image.file(File(image!.path)),
+                        TextButton(
+                          onPressed: getImage,
+                          child: Text(
+                            'Change image',
+                            style: Theme.of(context)
+                                .textTheme
+                                .subtitle2
+                                ?.copyWith(color: Colors.blueGrey),
+                          ),
+                        ),
+                      ],
+                    ),
+            ),
+            // TODO: ADD Profile Info Edit features
+          ],
         ));
+  }
+
+  Future<void> getImage() async {
+    XFile? imageReturned = await _picker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      image = imageReturned;
+    });
   }
 }
